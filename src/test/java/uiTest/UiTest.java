ad4ee7b53247b17b48697814a593c93786c03ca2
banancheg <ruslansolodovnyk@gmail.com>
2020-04-19 00:16:09 +0300

    [+] test jenkins

--- src/test/java/uiTest/UiTest.java
+++ src/test/java/uiTest/UiTest.java
@@ -6,7 +6,6 @@ import io.qameta.allure.Feature;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import ui.enums.DropdownItem;
 import ui.initialDriver.AppManager;
 import ui.listeners.Listener;
 
@@ -29,7 +28,7 @@ public class UiTest {
 
     @Test(description = "Open main page2")
     public void OpenMainPage2() throws InterruptedException {
-        appManager.getMainPageHelper().openMainPage().changeCityForCheckBalance(DropdownItem.SVIDIVOK.getText());
+        appManager.getMainPageHelper().openMainPage(); //.changeCityForCheckBalance(DropdownItem.SVIDIVOK.getText());
     }
 
 
