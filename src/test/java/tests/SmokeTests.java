cc9636dea6355dda1624b29a83ceb0c0df579faa
banancheg <ruslansolodovnyk@gmail.com>
2020-05-30 16:49:56 +0300

    [=] some changes for test testrail integration

--- src/test/java/tests/SmokeTests.java
+++ src/test/java/tests/SmokeTests.java
@@ -1,14 +1,10 @@
 package tests;
 
-import org.testng.annotations.Test;
-import utils.TestDataProviders;
-import utils.enums.Button;
-
 public class SmokeTests extends BaseTest{
 
-    @Test(description = "Відправка запиту на підключення", dataProvider = "getUserData", dataProviderClass = TestDataProviders.class)
+    /*@Test(description = "Відправка запиту на підключення", dataProvider = "getUserData", dataProviderClass = TestDataProviders.class)
     public void CheckSendingConnectRequestWithWrongTelephone(String surname, String name, String telephone, String address, String message, String expectedText){
         appManager.getMainPageHelper().openMainPage().clickOnButtonInHeader(Button.CONNECT.getText());
         appManager.getConnectPageHelper().fillConnectFormAndSendRequest(surname, name, telephone, address, message).compareTextAfterSendRequestForConnectWithWrongData(expectedText);
-    }
+    }*/
 }
